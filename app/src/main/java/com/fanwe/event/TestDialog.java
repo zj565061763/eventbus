6cb07d0a570c9cd8d3d4ab462bb296e103025091
zjun <565061763@qq.com>
2018-04-17 16:49:41 +0800

    demo

---
+++ app/src/main/java/com/fanwe/event/TestDialog.java
@@ -0,0 +1,27 @@
+package com.fanwe.event;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.util.Log;
+
+import com.fanwe.lib.eventbus.FEventObserver;
+
+public class TestDialog extends Dialog
+{
+    public static final String TAG = TestDialog.class.getSimpleName();
+
+    public TestDialog(Context context)
+    {
+        super(context);
+        setContentView(R.layout.dialog_test);
+    }
+
+    private FEventObserver<TestEvent> mEventObserver = new FEventObserver<TestEvent>()
+    {
+        @Override
+        public void onEvent(TestEvent event)
+        {
+            Log.i(TAG, "onEvent dialog:" + event);
+        }
+    }.setLifecycle(this);
+}
