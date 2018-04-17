8ffb854d983f0ef2afccfad363a304fd23dee6fd
zjun <565061763@qq.com>
2018-04-17 11:52:16 +0800

    demo

--- app/src/main/java/com/fanwe/event/App.java
+++ app/src/main/java/com/fanwe/event/App.java
@@ -2,8 +2,6 @@ package com.fanwe.event;
 
 import android.app.Application;
 
-import com.fanwe.lib.eventbus.FEventBus;
-
 /**
  * Created by Administrator on 2018/2/1.
  */
@@ -14,20 +12,5 @@ public class App extends Application
     public void onCreate()
     {
         super.onCreate();
-
-        /**
-         * 设置调试模式
-         */
-        FEventBus.getDefault().setDebug(true);
-
-        /**
-         * 发送一个粘性事件，当有对应事件的观察者注册的时候，会在注册的时候立即通知此事件
-         */
-        FEventBus.getDefault().postSticky(new TestEvent());
-
-        /**
-         * 移除粘性事件
-         */
-//        FEventBus.getDefault().removeSticky(TestEvent.class);
     }
 }
